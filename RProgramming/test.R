8f11397c3f2a3dac8941f3c87c4e86018300298b
Nallapu Naveen <naveennallapu750@gmail.com>
2025-12-07 10:15:36 +0000

    Test

---
+++ RProgramming/test.R
@@ -0,0 +1,42 @@
+numbers <- c(1,3,5,6,2,3)
+print(numbers)
+char <- c("nav","varma","chhc")
+bool <- c(TRUE,FALSE,TRUE)
+print(bool)
+
+mix_data <- c(1,"naveen",TRUE)
+print(mix_data)
+
+ x<- c(1,2,3,4)
+ y <- c(5,6,7,2)
+ print(x+y)
+
+ char[c(1,3)]
+ char[c(TRUE, FALSE, TRUE)]
+ 
+ substr(char[1],3,3,char[3],2,2)
+ 
+ 
+ z <- 1:10
+ print(z)
+ 
+ seq(from=2,to=10,by=3 )
+ seq(from=1, to=6, by=1.5)
+ 
+ seq(from=1, to=4,length.out=5)
+ 
+ x <- 1:6
+ x*2
+ 
+ (A <- matrix(1:8, ncol=4, byrow=TRUE))
+ 
+ diag(4)
+ 
+ A <- diag(c(1,2,3,4,5))
+ A
+ 
+ rbind(c(1,2,3,4), c(2,3,4,5))
+ 
+ 
+ 
+ 
